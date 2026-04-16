1a240b0652ce15fd61d609377f419ec1531fb203
Priyanshu Gupta <gpriyanshu995@gmail.com>
2026-04-17 02:09:58 +0530

    adde

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Asterisms Legal",
+  title: "Demo Law Firm",
   description: "Your site description here",
   icons: {
     icon: "/icon.png"
